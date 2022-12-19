d5a49ca91e7405d62759d69305f0689994e4b93a
trevor <trevor@modernintelligence.ai>
2022-12-19 11:42:47 -0600

    added center point and radius calculations, added fire station query and json load/save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # area-study
 
 Python program that will allows you to designate what you want marked on your area study and generates a kml layer for atak with the specified layers.
+
+Don't use the API to debug, use the pushed example files
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import argparse
 import configparser
 import os
 import typing
+import simplekml
+import geopy.distance
+import requests
+import json
 
 
 def run(
@@ -9,6 +13,7 @@ def run(
     data_directory: typing.Optional[str],
     save_directory: str,
     location: typing.List[float],
+    google_api_key: str,
     config_file: typing.Optional[str] = None,
     emergency_services: typing.Optional[bool] = None,
     law_enforcement: typing.Optional[bool] = None,
@@ -17,7 +22,46 @@ def run(
     power: typing.Optional[bool] = None,
     food: typing.Optional[bool] = None,
 ) -> None:
-    x = 1
+    kml = simplekml.Kml()
+    ground = kml.newgroundoverlay(name='GroundOverlay')
+    ground.latlonbox.north = float(location[0])
+    ground.latlonbox.south = float(location[2])
+    ground.latlonbox.east = float(location[3])
+    ground.latlonbox.west = float(location[1])
+    ground.latlonbox.rotation = 0
+
+    center_point = [(float(location[0]) + float(location[2])) / 2, (float(location[3]) + float(location[1])) / 2]
+    radius = geopy.distance.geodesic(center_point, [float(location[0]), float(location[1])]).m
+
+    file_name = save_directory + "/master_" + str(center_point[0]) + "_" + str(center_point[1]) + ".kml"
+
+    if emergency_services:
+        # pull fire stations
+        fire_file_name = save_directory + "/fire_" + str(center_point[0]) + "_" + str(center_point[1]) + ".json"
+        if os.path.isfile(fire_file_name):
+            with open(fire_file_name, 'r') as f:
+                fire_json = json.load(f)
+        else:
+            fire_api_request = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" \
+                          + str(center_point[0]) + "," + str(center_point[1]) + \
+                          "&radius=" + str(radius) + "&type=fire_station&sensor=true&key=" + str(google_api_key)
+            fire_response = requests.get(fire_api_request)
+            if not os.path.isfile(fire_file_name):
+                with open(fire_file_name, 'w') as f:
+                    json.dump(fire_response.json(), f)
+            fire_json = fire_response.json()
+
+        fire_stations = fire_json["results"]
+        kml_points = []
+        for station in fire_stations:
+            kml_points.append(kml.newpoint(name=station["name"],
+                                           coords=[(station["geometry"]["location"]["lat"],
+                                                    station["geometry"]["location"]["lng"])]))
+
+        kml.save(file_name)
+
+
+
 
 
 def main() -> None:
@@ -42,12 +86,19 @@ def main() -> None:
         "--config-file",
         required=False,
         type=str,
-        default=None,
+        default="config.ini",
         help="config for CLI arguments",
     )
     parser.add_argument(
+        "--google-api-key",
+        required=False,
+        type=str,
+        default=None,
+        help="google maps api key",
+    )
+    parser.add_argument(
         "--location",
-        required=True,
+        required=False,
         type=list,
         default=None,
         help="upper left, and lower right bounds of area study",
@@ -94,9 +145,17 @@ def main() -> None:
         if os.path.isfile(opt.config_file):
             config = configparser.ConfigParser()
             config.read(opt.config_file)
-            opt.classification = config["cutlass"].get("CLASSIFICATION")
-            opt.dataset_directory = config["cutlass"].get("DATASET_DIRECTORY")
-            opt.save_directory = config["cutlass"].get("SAVE_DIRECTORY")
+            opt.classification = config["area-study"].get("CLASSIFICATION")
+            opt.data_directory = config["area-study"].get("DATA_DIRECTORY")
+            opt.save_directory = config["area-study"].get("SAVE_DIRECTORY")
+            opt.google_api_key = config["area-study"].get("GOOGLE_API_KEY")
+            opt.location = config["area-study"].get("LOCATION").split(",")
+            opt.emergency_services = config["area-study"].get("EMERGENCY_SERVICES")
+            opt.law_enforcement = config["area-study"].get("LAW_ENFORCEMENT")
+            opt.military = config["area-study"].get("MILITARY")
+            opt.water = config["area-study"].get("WATER")
+            opt.power = config["area-study"].get("POWER")
+            opt.food = config["area-study"].get("FOOD")
         else:
             print("Config not found! Defaulting to cli arguments.")
 
